42fcc2c3608f4b20a86d51a3a09e8f446fae2cbe
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-02-13 11:24:50 +0100

    feat: add crud methods in workout session controller

--- src/main/java/se/jimmy/iths/todolist/controller/WorkoutSessionController.java
+++ src/main/java/se/jimmy/iths/todolist/controller/WorkoutSessionController.java
@@ -1,7 +1,60 @@
 package se.jimmy.iths.todolist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import se.jimmy.iths.todolist.model.WorkoutSession;
+import se.jimmy.iths.todolist.service.WorkoutSessionService;
 
 @Controller
+@RequestMapping("/workout")
 public class WorkoutSessionController {
+
+    private final WorkoutSessionService workoutSessionService;
+
+    public WorkoutSessionController(WorkoutSessionService workoutSessionService) {
+        this.workoutSessionService = workoutSessionService;
+    }
+
+    @GetMapping
+    public String getAllWorkouts(Model model) {
+        model.addAttribute("workoutSessions", workoutSessionService.getAllWorkoutSessions());
+        return "workout-list";
+    }
+
+    @GetMapping("/details/{id}")
+    public String showWorkoutDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("workoutSession", workoutSessionService.getById(id));
+        return "workout-detail";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("workoutSession", new WorkoutSession());
+        return "workout-create";
+    }
+
+    @PostMapping("/new")
+    public String createWorkout(@ModelAttribute WorkoutSession workoutSession) {
+        workoutSessionService.create(workoutSession);
+        return "redirect:/workout";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(Model model, @PathVariable Long id) {
+        model.addAttribute("workoutSession", workoutSessionService.getById(id));
+        return "workout-update";
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateWorkoutById(@PathVariable Long id, @ModelAttribute WorkoutSession workoutSession) {
+        workoutSessionService.update(workoutSession, id);
+        return "redirect:/workout";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteWorkout(@PathVariable Long id) {
+        workoutSessionService.delete(id);
+        return "redirect:/workout";
+    }
 }
